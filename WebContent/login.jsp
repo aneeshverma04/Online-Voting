917d98465cfce32410cc4ce19b08801242c280be
aneeshverma04 <aneeshvrma@yahoo.com>
2017-11-19 21:09:02 +0530

    Login button fixed at backend, frontend is messed up , front end to be done in html now?

--- WebContent/login.jsp
+++ WebContent/login.jsp
@@ -1,3 +1,4 @@
+<%@page import="com.voting.online.Login"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -7,5 +8,24 @@
 <title>Login</title>
 </head>
 <body>
+<jsp:useBean id="loginObject" class="com.voting.online.Login"></jsp:useBean>
+<jsp:setProperty property="*" name="loginObject"/>
+<% 
+	boolean val = loginObject.verify();
+	if(val) {
+		out.print("Login Successfull");
+		out.println("<br>");
+		session.setAttribute("Fname", Login.Fname);
+		session.setAttribute("Lname", Login.Lname);
+		out.print("Welcome : " + session.getAttribute("Fname") + " " + session.getAttribute("Lname"));		
+	}
+	else
+		out.println("Unsuccessfull");
+%> 
+<%--  <jsp:getProperty property="email" name="loginObject"/> 
+	this only for attributes of bean class
+--%>
+
+<jsp:forward page="Temp1.jsp"></jsp:forward>
 </body>
 </html>
--- src/com/voting/online/Login.java
+++ src/com/voting/online/Login.java
@@ -1,10 +1,14 @@
 package com.voting.online;
 
 import java.io.Serializable;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import com.mysql.jdbc.ResultSet;
 
 public class Login implements Serializable{
-private String email,password;
-
+private String email,psw;
+public static String Fname , Lname ;
 public String getEmail() {
 	return email;
 }
@@ -13,12 +17,48 @@ public void setEmail(String email) {
 	this.email = email;
 }
 
-public String getPassword() {
-	return password;
+public String getPsw() {
+	return psw;
 }
 
-public void setPassword(String password) {
-	this.password = password;
+public void setPsw(String psw) {
+	this.psw = psw;
+}
+public boolean verify() {
+	ResultSet resultset = null;
+	String temp = null;
+	String sql = "select Email from UserCredentials where Email='"+email+"'"; 
+	String pass	= "select * from UserCredentials where Email='"+email+"'"; 	
+	java.sql.Connection  con = null; // java.sql.Connection because className is also Connection
+									// if any other class name then 
+								    // Connection  con = null;
+	
+	try{
+		Class.forName("com.mysql.jdbc.Driver");		// mysql server
+		con =  DriverManager.getConnection("jdbc:mysql://localhost:3306/OnlineVoting","root","toor");
+		Statement stmt = con.createStatement();
+		boolean res = stmt.execute(sql);
+		if(res){
+			resultset = (ResultSet) stmt.executeQuery(pass);
+			while(resultset.next()) {
+				temp = resultset.getString("Password");
+				Fname = resultset.getString("FirstName");
+				Lname = resultset.getString("LastName");
+				}
+			if(temp.equals(psw))
+				return true;
+		}
+		else{
+            System.out.println("insertion failed");
+            con.close();
+            return false;
+		}
+	}
+	catch(Exception E){
+        System.err.println(E);
+	}
+	return false;
+		
 }
 
 }
